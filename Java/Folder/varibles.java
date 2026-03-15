5b927a19ed61b066a25b68819cfcb28edbafca9b
klakshmikuruba-pixel <klakshmikuruba@gmail.com>
2026-03-16 05:21:21 +0530

    Added code to files

---
+++ Java/Folder/varibles.java
@@ -0,0 +1,12 @@
+public class varibles {
+  
+    public static void main(String[] args) {
+
+        String name = "Lakshmi";
+        int age = 20;
+
+        System.out.println("My name is " + name);
+        System.out.println("My age is " + age);
+
+    }
+}           
